66f1b7e5940e303fb8e01e7e26099a3ef4668690
EasonFL <74353545+EasonFL@users.noreply.github.com>
2021-01-27 22:35:31 +0800

    Add files via upload

---
+++ Buy soda.c
@@ -0,0 +1,12 @@
+#define  _CRT_SECURE_NO_WARNINGS	
+#include<stdio.h>
+int main()
+{
+	int money;
+	printf("Input your money\n");
+	scanf("%d",&money);
+	printf("You can buy %d bottles of soda\n",money+money/2);
+
+
+
+}
