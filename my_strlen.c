547f04db80d37923b8b4b0e3007d9c9fd9757849
EasonFL <74353545+EasonFL@users.noreply.github.com>
2021-01-28 23:43:25 +0800

    Add files via upload

--- my_strlen.c
+++ my_strlen.c
@@ -1,23 +1,25 @@
 #define  _CRT_SECURE_NO_WARNINGS	
 #include<stdio.h>
-int my_strlen(char* str)
+#include<assert.h>
+int my_strlen(const char *str)
 {
-	if ((*str) != '\0')
+	assert(str != NULL);
+	int i = 0;
+	while (*str)
 	{
-		return 1 + my_strlen(str + 1);
+		i++;
+		str++;
 	}
-	else
-		return 0;
+	return i;
+
 }
+
+
 int main()
 {
-	char arr[] = "asdasd";
-	/*scanf("%s", arr);*/
-	int num = my_strlen(arr);
-	puts("The lenth is :\n");
+	char ch[30];
+	gets(ch);
+	int num = my_strlen(ch);
 	printf("%d\n",num);
-	return 0;
-
-
 
 }
