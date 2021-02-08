bab9e6293bef36aec521985347ba03ca9f89276d
EasonFL <74353545+EasonFL@users.noreply.github.com>
2021-02-08 23:06:05 +0800

    Add files via upload

--- reverse_string.c
+++ reverse_string.c
@@ -1,20 +1,46 @@
 #define  _CRT_SECURE_NO_WARNINGS	
 #include<stdio.h>
 #include<string.h>
-void reverse(char*str, int sz)
+//void reverse_string(char *arr, int k)
+//{
+//	
+//	int j = 0;
+//	int len = strlen(arr);
+//	for (j = 0; j < k; j++)
+//	{
+//		char ch = *arr;
+//		int i = 0;
+//		for ( i = 0; i<len-1; i++)
+//		{
+//			arr[i] = arr[i + 1];
+//		}
+//		arr[len - 1] = ch;
+//	}
+//}
+void reverse(char *e1, char *e2)
 {
-	while (sz>0)
+	while (e1 < e2)
 	{
-		printf("%c", *(str + sz - 1));
-		sz--;
+		char tmp = *e1;
+		*e1 = *e2;
+		*e2 = tmp;
+		e1++;
+		e2--;
 	}
+}
+void reverse_string(char arr[], int k)
+{
+	int len = strlen(arr);
+	reverse(arr,arr + k - 1);
+	reverse(arr + k, arr + len - 1);
+	reverse(arr,arr + len - 1);
+
 }
 int main()
 {
-	char ch[30];
-	gets(ch);
-	int sz = strlen(ch);
-	reverse(ch, sz);
-	return 0;
-
+	char arr[] = "abcdefgh";
+	int k = 3;
+	printf("%s\n",arr);
+	reverse_string(arr, k);
+	printf("%s\n",arr);
 }
