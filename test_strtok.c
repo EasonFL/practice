552331c7308874ae44ea8f74905c24f35be19f4e
EasonFL <74353545+EasonFL@users.noreply.github.com>
2021-02-24 23:10:39 +0800

    Add files via upload

---
+++ test_strtok.c
@@ -0,0 +1,31 @@
+
+#define  _CRT_SECURE_NO_WARNINGS	
+#include<stdio.h>
+//#include<string.h>
+//#include <stdio.h>
+#include <string.h>
+int main()
+{
+	char str[] = "- This, a simple string.";
+	char * pch;
+	printf("Splitting string \"%s\" into tokens:\n", str);
+	pch = strtok(str, " ,.-");
+	while (pch != NULL)
+	{
+		printf("%s\n", pch);
+		pch = strtok(NULL, " ,.-");
+	}
+	return 0;
+}
+//int main()
+//{
+//	char *p = "zhangpengwei@bitedu.tech";
+//	const char* sep = ".@";
+//	char arr[30];
+//	char *str = NULL;
+//	strcpy(arr, p);//将数据拷贝一份，处理arr数组的内容
+//	for (str = strtok(arr, sep); str != NULL; str = strtok(NULL, sep))
+//	{
+//		printf("%s\n", str);
+//	}
+//}
